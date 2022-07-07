5f58e667b2500bacd3b1f3e1e06681eed2583b5d
icabette <icabette@gmail.com>
2022-07-08 01:07:00 +0900

    버스 3대 모두 메일로 전송

--- Bus_Stop.py
+++ Bus_Stop.py
@@ -4,10 +4,10 @@ import smtplib
 from email.mime.text import MIMEText
 import pw
  
-def callAPI():
+def callAPI(bstopid):
     url = 'http://apis.data.go.kr/6260000/BusanBIMS/stopArrByBstopid'
     params ={'serviceKey' : 'JcfoCADRdvuoKVQ+BWTGO6Ztqm5WugNb03CCD4dzS6fm1mxpCe5lM3JLPdnONqoWeMp7W9+UbHRxf+/QuMycWQ==', \
-    'bstopid' : '175830201' }
+    'bstopid' : bstopid }
 
     response = requests.get(url, params=params)
     content = response.text
@@ -36,9 +36,9 @@ def sendEmail(busInfo):
     smtp.login(pw.id, pw.password)
     
     for i in busInfo:
-        msgBody = msgBody + busInfo[0] + "번 버스 도착까지 " + busInfo[1] + "분 남았습니다. 다음 버스는 " + busInfo[2] + "분 후에 도착합니다."
+        msgBody = msgBody + i[0] + "번 버스 도착까지 " + i[1] + "분 남았습니다. 다음 버스는 " + i[2] + "분 후에 도착합니다."
     msg = MIMEText(msgBody)
-    msg['Subject'] = '51번 버스 도착 시간 안내'
+    msg['Subject'] = '버스 도착 시간 안내'
     msg['From'] = pw.email
     msg['To'] = pw.email
     smtp.sendmail(pw.email, pw.email, msg.as_string())
@@ -46,8 +46,11 @@ def sendEmail(busInfo):
 
 def main():
     #root = fromFile()
-    root = callAPI()
-    busInfo = searchBusInfo(root)
+    bstopid = ['175830201','175830301']
+    busInfo = []
+    for i in bstopid:
+        root = callAPI(i)
+        busInfo.append(searchBusInfo(root))
     sendEmail(busInfo)
     
 main()
